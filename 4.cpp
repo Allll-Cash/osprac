86c2de693087f2b67f0dd136035f32e316597784
Allll-Cash <Liddell_ll@list.ru>
2021-02-08 03:32:34 +0300

    Задача 4

---
+++ 4.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+ 
+int main(void) 
+{
+    switch(fork())
+  {
+        case -1:
+            perror("fork");
+            return 1;
+        case 0:
+            if ( execl("/usr/bin/man", "man", "execl", NULL) == -1 )
+           {
+                perror("execl");
+                return 1;
+            }
+            break;
+        default:
+            wait(NULL);
+            printf("End program!");
+            break;
+    }
+    
+    return 0;
+}
